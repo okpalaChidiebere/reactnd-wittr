c7ac2739f33ff527371ee8d7fc32aa75e497b68f
okpalaChidiebere <okpalacollins4@gmail.com>
2023-05-07 10:20:45 -0600

    Hijacking Requests 2
    We return a promise as response.
    
    Notice: that we are responding with an image from the cache

--- wittr/public/sw.js
+++ wittr/public/sw.js
@@ -3,10 +3,9 @@ self.addEventListener("fetch", (event) => {
   //we are high jacking fetch request response and responding with a html
   //this is very powerful that why it is required to website to run service worker on HTTPS
   //Since service worker lasts way more that web pages, if a hacker get hold of our sw from an insecure connection they can change contents of our pages which we don't want :)
-  event.respondWith(
-    new Response(`Hello <b class="a-winner-is-me">world</b>`, {
-      status: 200,
-      headers: { "Content-Type": "text/html" },
-    })
-  );
+
+  //only respond to requests with a url ending in ".jpg"
+  if (event.request.url.endsWith(".jpg")) {
+    event.respondWith(fetch("imgs/dr-evil.gif")); //respond with an image from the cache
+  }
 });
