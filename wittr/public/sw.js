230a6f860fb726f978e9a1ae3a85792642de64c0
okpalaChidiebere <okpalacollins4@gmail.com>
2023-05-07 13:21:41 -0600

    Hijacking Requests 3

--- wittr/public/sw.js
+++ wittr/public/sw.js
@@ -4,8 +4,19 @@ self.addEventListener("fetch", (event) => {
   //this is very powerful that why it is required to website to run service worker on HTTPS
   //Since service worker lasts way more that web pages, if a hacker get hold of our sw from an insecure connection they can change contents of our pages which we don't want :)
 
-  //only respond to requests with a url ending in ".jpg"
-  if (event.request.url.endsWith(".jpg")) {
-    event.respondWith(fetch("imgs/dr-evil.gif")); //respond with an image from the cache
-  }
+  event.respondWith(handle404(event.request));
 });
+
+const handle404 = async (request) => {
+  try {
+    const responseFromNetwork = await fetch(request);
+
+    if (responseFromNetwork.status === 404) {
+      return fetch("imgs/dr-evil.gif");
+    }
+
+    return responseFromNetwork;
+  } catch (error) {
+    return new Response("Uh oh, that totally failed!");
+  }
+};
